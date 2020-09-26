78feaa24a688d21cf25c9195cd1f8f477b9740f7
Isatou D Sanyang <isasanyang1986@yahoo.co.uk>
2020-09-26 15:25:49 -0500

    adding files

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require("express");
+const fs = require("fs");
+
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000 
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/assets", express.static("./assets"));
+
+
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
